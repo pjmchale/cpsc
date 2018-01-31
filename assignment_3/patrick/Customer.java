340d4b7fd3dc2a84d6a04fe87a39fc90bc7f505b
pjmchale <pjmchale@ucalgary.ca>
2018-01-31 12:19:32 -0700

    added trace etc

--- assignment_3/patrick/Customer.java
+++ assignment_3/patrick/Customer.java
@@ -1,10 +1,18 @@
 
 public class Customer{
-  String name;
-  int customerID;
+  private String name;
+  private int customerID;
 
   /**
-   * Constructor for class. Sets name and id of customer
+   * Constructor for class with no arguments
+   */
+  Customer(){
+    name = "No Name";
+    customerID = 0;
+  }
+
+  /**
+   * Constructor for class, with anme and id arguments.
    * @param customerName name to set customer to
    * @param id id to set customerID to
    */
@@ -14,10 +22,20 @@ public class Customer{
   }
 
   /**
+   * Constructor for class with oldCustomer input
+   * @param customerName name to set customer to
+   * @param id id to set customerID to
+   */
+  Customer(Customer oldCustomer){
+    name = oldCustomer.getName();
+    customerID = oldCustomer.getID();
+  }
+
+  /**
    * Returns the customers name
    * @return name
    */
-  String getName(){
+  public String getName(){
     return name;
   }
 
@@ -25,7 +43,7 @@ public class Customer{
    * Sets the customer name
    * @param newName name to change customer name to
    * */
-  void setName(String newName){
+  public void setName(String newName){
     name = newName;
   }
 
@@ -33,7 +51,7 @@ public class Customer{
    * Returns the customer ID
    * @return customerID
    */
-  int getCustomerID(){
+  public int getID(){
     return customerID;
   }
 
@@ -41,25 +59,19 @@ public class Customer{
    * Sets the customer ID
    * @param id input id to set customer ID to
    */
-  void setCustomerID(int id){
+  public void setID(int id){
     customerID = id;
   }
 
+  /**
+   * Returns formatted string describing values of instance variables
+   */
   public String toString(){
     return "Customer Name: " + name + ", Customer ID: " + customerID;
   }
 
-  public static void main(String[] args){
-    new Trace();
-  }
 
 }
 
 
-class Trace{
 
-  Trace(){
-    BankAccount b1 = new BankAccount();
-  }
-
-}
