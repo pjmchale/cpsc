7e851df2b19abe82ecb33b2623514f8aeb6b6efc
pjmchale <pjmchale@ucalgary.ca>
2018-02-02 12:28:32 -0700

    initial commit of GameManager and Main

---
+++ MainProject/GameManager.java
@@ -0,0 +1,28 @@
+
+public class GameManager{
+  int numPlayers;
+  Map map;
+  Player turn;
+  int turnIndex;
+
+  void initGameState(){
+
+  } 
+
+  void nextTurn(){
+
+  }
+
+  boolean isGameOver(){
+
+  }
+
+  void exitGame(){
+
+  }
+
+  void playTurn(){
+
+  }
+
+}
