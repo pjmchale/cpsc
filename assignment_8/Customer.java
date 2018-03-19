b97c82934c45c52c41c7aeb63b351b61c84096ac
pjmchale <pjmchale@zone07-ec.cs.ucalgary.ca>
2018-03-19 10:16:21 -0600

    added assignment 8

---
+++ assignment_8/Customer.java
@@ -0,0 +1,77 @@
+
+final public class Customer{
+  private String name;
+  private int customerID;
+
+  /**
+   * Constructor for class with no arguments
+   */
+  Customer(){
+    name = "No Name";
+    customerID = 0;
+  }
+
+  /**
+   * Constructor for class, with anme and id arguments.
+   * @param customerName name to set customer to
+   * @param id id to set customerID to
+   */
+  Customer(String customerName, int id){
+    name = customerName;
+    customerID = id;
+  }
+
+  /**
+   * Constructor for class with oldCustomer input
+   * @param customerName name to set customer to
+   * @param id id to set customerID to
+   */
+  Customer(Customer oldCustomer){
+    name = oldCustomer.getName();
+    customerID = oldCustomer.getID();
+  }
+
+  /**
+   * Returns the customers name
+   * @return name
+   */
+  public String getName(){
+    return name;
+  }
+
+  /**
+   * Sets the customer name
+   * @param newName name to change customer name to
+   * */
+  public void setName(String newName){
+    name = newName;
+  }
+
+  /**
+   * Returns the customer ID
+   * @return customerID
+   */
+  public int getID(){
+    return customerID;
+  }
+
+  /**
+   * Sets the customer ID
+   * @param id input id to set customer ID to
+   */
+  public void setID(int id){
+    customerID = id;
+  }
+
+  /**
+   * Returns formatted string describing values of instance variables
+   */
+  public String toString(){
+    return "Customer Name: " + name + ", Customer ID: " + customerID;
+  }
+
+
+}
+
+
+
