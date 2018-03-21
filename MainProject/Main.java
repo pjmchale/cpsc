081165f75d997cb8c2435e1cebfb21f2ba119c18
pjmchale <pjmchale@ucalgary.ca>
2018-03-21 12:27:36 -0600

    added main method

---
+++ MainProject/Main.java
@@ -0,0 +1,11 @@
+/**
+ * This is the main method to launch the RISK Game GUI
+ */
+import GameEngine.*;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(MainGUI.class, args);
+    }
+}
